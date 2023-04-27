374a5c865820e99f159ff4f8b8bb26915308f513
ABiviano13 <abiviano0@outlook.com>
2023-04-27 17:29:20 +0200

    FEAT relazione oneToMany completed

--- app/Models/Type.php
+++ app/Models/Type.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Type extends Model
 {
     use HasFactory;
+
+    public function projects(){
+        
+        return $this->hasMany(Project::class); //non serve importare il model poichè si trova nella stessa cartella.
+    }
 }
