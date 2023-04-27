facbedeb924c9a1e5dbed2d124c51e7c67de11bc
ABiviano13 <abiviano0@outlook.com>
2023-04-27 17:42:52 +0200

    FEAT type in project db

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Project;
+use App\Models\Type;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
@@ -17,12 +18,15 @@ class ProjectSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
+        $type_ids = Type::all()->pluck('id')->all();
+
         for($i=0; $i < 100; $i++){
 
             $project = new Project();
             $project->title = $faker->unique()->sentence(4);
             $project->content = $faker->optional()->text(350);
             $project->slug = Str::slug($project->title, '-');
+            $project->type_id = $faker->optional()->RandomElement($type_ids);
 
             $project->save();
 
